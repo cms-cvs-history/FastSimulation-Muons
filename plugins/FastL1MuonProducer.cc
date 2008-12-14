9d9a17ebbd2f8749d9b91e387b305e13985f9d53
Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch>
2008-12-14 16:19:00 +0000

    Andrea... Why didn't you initialize the RPC geometry ? mmmhhh ?

--- plugins/FastL1MuonProducer.cc
+++ plugins/FastL1MuonProducer.cc
@@ -464,6 +464,8 @@ FastL1MuonProducer::beginJob(const edm::EventSetup& es)
   es.get<MuonGeometryRecord>().get(dtGeometry);
 // Get the CSC Geometry
   es.get<MuonGeometryRecord>().get(cscGeometry);
+// Get the RPC Geometry
+  es.get<MuonGeometryRecord>().get(rpcGeometry);
 
 }
 
